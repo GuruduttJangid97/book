e5bfd940fb47caa7428f2c104f277d79efe3092b
GURUDUTT JANGID <sharmagurudutt9950@hotmail.com>
2022-02-23 22:43:51 +0530

    files added

---
+++ precache-manifest.e7f744b657e929c4cb6d18b07cf8d96e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc40a190ad9258cd7528f88fc091ee9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4a8484d984a6721eff3",
+    "url": "/static/css/main.868ac4a1.chunk.css"
+  },
+  {
+    "revision": "fb9cde2665697c38246a",
+    "url": "/static/js/2.1b03a28e.chunk.js"
+  },
+  {
+    "revision": "cbea6b49bf20b66d679cafffb983c7af",
+    "url": "/static/js/2.1b03a28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a8484d984a6721eff3",
+    "url": "/static/js/main.c93d2e8c.chunk.js"
+  },
+  {
+    "revision": "eacbad01525e16a056fe",
+    "url": "/static/js/runtime-main.1b2332cd.js"
+  }
+]);
